bff01794b31ea12b47437b35c2ef8eba1f141a7c
acarro84 <123746711+acarro84@users.noreply.github.com>
2023-06-27 17:25:29 -0500

    first push to merge with dauns main. Added some model functionality

--- backend/src/main/java/com/skillstorm/models/Item.java
+++ backend/src/main/java/com/skillstorm/models/Item.java
@@ -1,16 +1,82 @@
+package com.skillstorm.models;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 @Entity
 public class Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    // Add other fields
+	private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "warehouse_id")
-    private Warehouse warehouse;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "warehouse_id")
+	private int warehouseId;
+	private String type;
+	private String soldBy;
 
-    // Add constructors, getters, and setters
+	public Item() {
+		super();
+	}
+
+	public Item(Long id, String name, int warehouseId, String type, String soldBy, Warehouse warehouse) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.warehouseId = warehouseId;
+		this.type = type;
+		this.soldBy = soldBy;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWarehouseId() {
+		return warehouseId;
+	}
+
+	public void setWarehouseId(int warehouseId) {
+		this.warehouseId = warehouseId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSoldBy() {
+		return soldBy;
+	}
+
+	public void setSoldBy(String soldBy) {
+		this.soldBy = soldBy;
+	}
+
+	
+
 }
